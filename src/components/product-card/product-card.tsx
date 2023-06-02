5b85ae39befc998d3d0454f9aea1f445b27199a8
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-06-02 18:46:42 +0700

    доработка карточки каталога

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,50 +1,55 @@
+import { AppRoute } from '../../router/constants';
 import { Product } from '../../store/products-api/types';
+import { Link, generatePath } from 'react-router-dom';
 
 type ProductCardProps = {
   camera: Product;
 }
 
-const ProductCard = ({camera}:ProductCardProps) => (
-  <div className="product-card">
-    <div className="product-card__img">
-      <picture>
-        <source type="image/webp" srcSet={`${camera.previewImgWebp}, ${camera.previewImgWebp2x}`} />
-        <img src={camera.previewImg} srcSet={camera.previewImg2x} width="280" height="240" alt={camera.name} />
-      </picture>
-    </div>
-    <div className="product-card__info">
-      <div className="rate product-card__rate">
-        <svg width="17" height="16" aria-hidden="true">
-          <use xlinkHref="#icon-full-star"></use>
-        </svg>
-        <svg width="17" height="16" aria-hidden="true">
-          <use xlinkHref="#icon-full-star"></use>
-        </svg>
-        <svg width="17" height="16" aria-hidden="true">
-          <use xlinkHref="#icon-full-star"></use>
-        </svg>
-        <svg width="17" height="16" aria-hidden="true">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-        <svg width="17" height="16" aria-hidden="true">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-        <p className="visually-hidden">Рейтинг: 3</p>
-        <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
-      </div>
-      <p className="product-card__title">{camera.name}</p>
-      <p className="product-card__price"><span className="visually-hidden">Цена:</span>{camera.price} ₽
-      </p>
-    </div>
-    <div className="product-card__buttons">
-      <button className="btn btn--purple product-card__btn" type="button">
-        Купить
-      </button>
-      <a className="btn btn--transparent" href="#">
-        Подробнее
-      </a>
-    </div>
-  </div>
-);
+const ProductCard = ({camera}:ProductCardProps) => {
+  const link = generatePath(AppRoute.Product, {
+    id: `${camera?.id || ''}`,
+  });
 
+  return (
+    <div className="product-card">
+      <div className="product-card__img">
+        <picture>
+          <source type="image/webp" srcSet={`${camera.previewImgWebp}, ${camera.previewImgWebp2x}`} />
+          <img src={camera.previewImg} srcSet={camera.previewImg2x} width="280" height="240" alt={camera.name} />
+        </picture>
+      </div>
+      <div className="product-card__info">
+        <div className="rate product-card__rate">
+          <svg width="17" height="16" aria-hidden="true">
+            <use xlinkHref="#icon-full-star"></use>
+          </svg>
+          <svg width="17" height="16" aria-hidden="true">
+            <use xlinkHref="#icon-full-star"></use>
+          </svg>
+          <svg width="17" height="16" aria-hidden="true">
+            <use xlinkHref="#icon-full-star"></use>
+          </svg>
+          <svg width="17" height="16" aria-hidden="true">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+          <svg width="17" height="16" aria-hidden="true">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+          <p className="visually-hidden">Рейтинг: 3</p>
+          <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
+        </div>
+        <p className="product-card__title">{camera.name}</p>
+        <p className="product-card__price"><span className="visually-hidden">Цена:</span>{camera.price} ₽
+        </p>
+      </div>
+      <div className="product-card__buttons">
+        <button className="btn btn--purple product-card__btn" type="button">
+        Купить
+        </button>
+        {camera?.id && <Link className="btn btn--transparent" to={link}>Подробнее</Link>}
+      </div>
+    </div>
+  );
+};
 export default ProductCard;
