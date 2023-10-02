a2a0a61bb482629841e8627205c982fa975bf4a8
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-10-02 18:33:58 +0700

    fix

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -4,10 +4,10 @@ import { LoadingScreen } from '../loading-screen';
 import { promoApi } from '../../store/promo-api/promo-api';
 
 export const Banner = () => {
-  const {data} = promoApi.useGetListQuery();
+  const { data } = promoApi.useGetListQuery();
 
-  if(!data){
-    return <LoadingScreen/>;
+  if (!data) {
+    return <LoadingScreen />;
   }
 
   const link = generatePath(AppRoute.Product, {
@@ -18,12 +18,14 @@ export const Banner = () => {
     <div className="banner">
       <picture>
         <source type="image/webp" srcSet={`${data.previewImgWebp || ''}, ${data.previewImgWebp2x || ''}`} />
-        <img src={data.previewImg} srcSet={data.previewImg2x} width="1280" height="280" alt="баннер" />
+        {data.id ? <img src={data.previewImg} srcSet={data.previewImg2x} width="1280" height="280" alt="баннер" />
+          :
+          <img src='/img/content/banner-bg.jpg' srcSet={data.previewImg2x} width="1280" height="280" alt="баннер" />}
       </picture>
       <p className="banner__info"><span className="banner__message">Новинка!</span>
         <span className="title title--h1">{data.name}</span>
         <span className="banner__text">
-    Профессиональная камера от&nbsp;известного производителя
+          Профессиональная камера от&nbsp;известного производителя
         </span>
         {data.id && <Link className="btn" to={link}>Подробнее</Link>}
       </p>
